07ad8da917e17753d8546a64e99686f5eae19b51
Sunnatullox <89123707+Sunnatullox@users.noreply.github.com>
2022-02-05 22:42:42 +0500

    end app

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
   return (
     <nav className="">
       <div
-        className="flex px-10 overflow-y-hidden sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
+        className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
       >
         {Object.entries(request).map(([key, { title, url }]) => (
           <h2
